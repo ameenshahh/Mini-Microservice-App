6a13fbff18bf415dd43c4f34a6b38614904af26a
Ameen Shah <ameenshahshahjahan@gmail.com>
2023-11-06 21:53:39 +0530

    created event bus express server

---
+++ event-bus/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+// const cors = require('cors')
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const { randomBytes } = require("crypto");
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+
+  res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+  console.log(`listening on PORT 4005`);
+});
