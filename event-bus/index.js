b0d29818d319a9917d16c30de897b1fa0d343558
Ameen Shah <ameen@zotok.ai>
2024-10-27 20:51:33 +0530

    event bus setup completed

--- event-bus/index.js
+++ event-bus/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 // const cors = require('cors')
-const axios = require('axios')
-
+const axios = require("axios");
 
 app.use(bodyParser.json());
 // app.use(cors());
@@ -16,20 +15,40 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-  axios.post("http://localhost:4003/events", event);
+  events.push(event);
+
+  //posts
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  //comments
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  //query
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+
+  //moderation
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
   console.log(`listening on PORT 4005`);
 });
-
-
-
